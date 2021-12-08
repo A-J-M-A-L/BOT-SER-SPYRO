de95b85a896a6018b42b7ea918bd2a864b890271
spyro-ser-ofc <92428473+spyro-ser-ofc@users.noreply.github.com>
2021-12-08 12:02:08 +0530

    Update Bosco.js

--- Bosco.js
+++ Bosco.js
@@ -1339,27 +1339,50 @@ wa.me/${owner}`
             fgclink('*BOT OFFLINE*')
             break   
          case 'tobc':
+
 				if (!isOwner) return reply(mess.only.ownerB)
+
 				if (args.length < 1) return reply('𝚃𝙴𝚇𝚃?')
+
 				var bc = body.slice(6)
+
 					bosco.updatePresence(from, Presence.composing)
+
 					anu = await bosco.chats.all()
+
 					if (isMedia && !mek.message.videoMessage || isQuotedAudio) {
+
 						const encmedia = isQuotedAudio ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+
 						buff = await bosco.downloadMediaMessage(encmedia)
+
 						for (let _ of anu) {
-							bosco.sendMessage(_.jid, buff, audio, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})}, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./ds.jpg'), surface: 200, message: `${bc}`, orderTitle: `${bc}`, sellerJid: '0@s.whatsapp.net'}}}, contextInfo: { forwardingScore: 508, isForwarded: true}, mimetype: 'audio/mp4', duration: 359996400, ptt: true })
+
+							bosco.sendMessage(_.jid, buff, audio, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})}, message: { orderMessage: { itemCount: 9999999999, status: 200, thumbnail: fs.readFileSync('./ds.jpg'), surface: 200, message: `${bc}`, orderTitle: `${bc}`, sellerJid: '0@s.whatsapp.net'}}}, contextInfo: { forwardingScore: 508, isForwarded: true}, mimetype: 'audio/mp4', duration: 7356663773663, ptt: true })
+
 						}
+
 						} else if (isMedia && !mek.message.videoMessage || isQuotedSticker) {
+
 						const encmedia = isQuotedSticker ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+
 						buff = await bosco.downloadMediaMessage(encmedia)
+
 						for (let _ of anu) {
-							bosco.sendMessage(_.jid, buff, sticker, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})}, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./ds.jpg'), surface: 200, message: `${bc}`, orderTitle: `${bc}`, sellerJid: '0@s.whatsapp.net'}}}, contextInfo: { forwardingScore: 508, isForwarded: true}})
+
+							bosco.sendMessage(_.jid, buff, sticker, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})}, message: { orderMessage: { itemCount: 9999999999, status: 200, thumbnail: fs.readFileSync('./ds.jpg'), surface: 200, message: `${bc}`, orderTitle: `${bc}`, sellerJid: '0@s.whatsapp.net'}}}, contextInfo: { forwardingScore: 508, isForwarded: true}})
+
 							}
+
 							} else{
+
           reply('reply sticker/audio')
+
 							}
+
 					break
+							
+
                case 'restart':
 if (!isOwner && !mek.key.fromMe) return reply(mess.only.owner)
 reply(`*Restarting*`)
